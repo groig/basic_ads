15299e97a722b925aba44bd9445d1b59c1f52f57
Roig <groig@protonmail.com>
2020-05-06 12:04:06 -0400

    heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :basic_ads, BasicAdsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "aqueous-spire-31090.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
