5880fe8091d8892c84f154417d6911103edd8616
Roig <groig@protonmail.com>
2020-05-08 19:35:33 -0400

    add timex

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule BasicAds.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:timex, "~> 3.5"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
